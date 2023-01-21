e389067935d483091c2a899d2e6c6733a4455acc
TharunSaravanan <bstharun@gmail.com>
2023-01-21 10:55:00 -0600

    Revert "date format"
    This reverts commit 7ee95df47d9cff4a0e844015129f2b9ae828f8e8.

--- src/main/java/com/fbla/fblaproject/model/Event.java
+++ src/main/java/com/fbla/fblaproject/model/Event.java
@@ -5,7 +5,6 @@
  */
 package com.fbla.fblaproject.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
