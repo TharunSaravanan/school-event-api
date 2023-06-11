afaa01768fcd969a8f9e61a7143efe3d5d1ddb82
Tharun Saravanan <bstharun@gmail.com>
2023-06-11 17:57:35 -0500

    fix leaderboard

--- src/main/java/com/fbla/fblaproject/repository/PointRepository.java
+++ src/main/java/com/fbla/fblaproject/repository/PointRepository.java
@@ -29,7 +29,8 @@ public interface PointRepository extends JpaRepository<Point, Integer> {
                     "WHERE p.student_id = s.id\n" +
                     "AND p.event_id = e.id\n" +
                     "AND e.time >= (select start_date from quarter where name = ?1)\n" +
-                    "group by student_id, student_grade, points\n" +
+                    "AND e.time <= (select end_date from quarter where name = ?1)\n" +
+                    "group by student_id, student_grade\n" +
                     "order by StudentGrade, TotalPoints desc", nativeQuery = true)
     List<LeaderBoardPoint> getLeaderBoardForQuarter(String quarterName);
 }
